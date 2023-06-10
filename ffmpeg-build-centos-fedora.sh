2b69bbd99f426b3c996da0a421505089a839275e
amidevous <amidevous@users.noreply.github.com>
2023-06-10 13:21:14 +0200

    Update ffmpeg-build-centos-fedora.sh

--- ffmpeg-build-centos-fedora.sh
+++ ffmpeg-build-centos-fedora.sh
@@ -1,60 +1,3 @@
-#!/bin/bash
-echo -e "\nChecking that minimal requirements are ok"
-# Ensure the OS is compatible with the launcher
-if [ -f /etc/centos-release ]; then
-    inst() {
-       rpm -q --queryformat '%{Version}-%{Release}' "$1"
-    } 
-    if (inst "centos-stream-repos"); then
-    OS="CentOS-Stream"
-    else
-    OS="CentOs"
-    fi    
-    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/centos-release) VER=${VERFULL:0:1} # return 6, 7, 8, 9 etc
-elif [ -f /etc/fedora-release ]; then
-    inst() {
-       rpm -q --queryformat '%{Version}-%{Release}' "$1"
-    } 
-    OS="Fedora" VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/fedora-release) VER=${VERFULL:0:2} # return 34, 35, 36,37 etc
-elif [ -f /etc/lsb-release ]; then
-    OS=$(grep DISTRIB_ID /etc/lsb-release | sed 's/^.*=//') VER=$(grep DISTRIB_RELEASE /etc/lsb-release | sed 's/^.*=//')
-	inst() {
-       dpkg-query --showformat='${Version}' --show "$1"
-    }
-elif [ -f /etc/os-release ]; then
-    OS=$(grep -w ID /etc/os-release | sed 's/^.*=//') VER=$(grep VERSION_ID /etc/os-release | sed 's/^.*"\(.*\)"/\1/' | head -n 1 | tail -n 1)
- else
-    OS=$(uname -s) VER=$(uname -r)
-fi
-ARCH=$(uname -m)
-echo "Detected : $OS  $VER  $ARCH"
-# this part must be updated every 6 months
-if [[ "$OS" = "CentOs" && "$VER" = "7" && "$ARCH" == "x86_64" || "$OS" = "CentOS-Stream" && "$VER" = "8" && "$ARCH" == "x86_64" ||
-"$OS" = "CentOS-Stream" && "$VER" = "9" && "$ARCH" == "x86_64" || "$OS" = "Fedora" && "$VER" = "37" && "$ARCH" == "x86_64" ||
- "$OS" = "Fedora" && "$VER" = "38" && "$ARCH" == "x86_64"  ]] ; then
-    echo "Ok."
-else
-    echo "Sorry, this OS is not supported."
-	echo "This script is online for Linux x86_64 Stable Version"
-	echo "Only aviable for :"
-	echo "Centos Version 7"
-	echo "CentOS Stream Version 8"
-	echo "CentOS Stream Version 9"
-	echo "Fedora Version 37"
-	echo "Fedora Version 38"
-    exit 1
-fi
-# define Package Variable
-if [[ "$OS" = "CentOs" ]] ; then
-		yum -y install epel-release
-		yum -y update
-		yum -y install dnf
-    dnf -y install centos-release-scl
-    dnf -y install devtoolset-8
-fi
-dnf -y install rpm-build make git gcc gcc-c++ gcc-gfortran gcc-objc gcc-objc++ libstdc++-devel \
-autoconf automake libtool wget bzip2 gzip xz wget tar make pkgconfig patch m4 coreutils
-rm -rf $(rpm --eval %_topdir)/SPECS $(rpm --eval %_topdir)/SOURCES
 mkdir -p $(rpm --eval %_topdir)/SPECS
 mkdir -p $(rpm --eval %_topdir)/SOURCES
 wget https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.xz -O $(rpm --eval %_topdir)/SOURCES/gmp-6.2.1.tar.xz
@@ -92,6 +35,8 @@ dnf -y install $(find $(rpm --eval %_topdir)/RPMS -name 'xtreamui-zlib-1.2.13-3.
 
 
 
+
+
 wget https://github.com/silnrsi/teckit/releases/download/v2.5.11/teckit-2.5.11.tar.gz -O $(rpm --eval %_topdir)/SOURCES/teckit-2.5.11.tar.gz
 wget https://raw.githubusercontent.com/amidevous/ffmpegbuild/main/SPECS/xtreamui-gettext/xtreamui-teckit.spec -O $(rpm --eval %_topdir)/SPECS/xtreamui-teckit.spec
 
@@ -114,75 +59,6 @@ wget https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.1-disable-libtextstyle.pat
 wget https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.1-covscan.patch -O $(rpm --eval %_topdir)/SOURCES/gettext-0.21.1-covscan.patch
 wget https://ftp.gnu.org/pub/gnu/gettext/gettext-java17-2062407.patch -O $(rpm --eval %_topdir)/SOURCES/gettext-java17-2062407.patch
 wget https://raw.githubusercontent.com/amidevous/ffmpegbuild/main/SPECS/xtreamui-gettext/xtreamui-gettext.spec -O $(rpm --eval %_topdir)/SPECS/xtreamui-gettext.spec
-
-
-
-
-
-
-
-
-
-
-
-
-
-wget https://ftp.gnu.org/gnu/nettle/nettle-3.8.tar.gz -O $(rpm --eval %_topdir)/SOURCES/nettle-3.8.tar.gz
-wget https://raw.githubusercontent.com/amidevous/ffmpegbuild/main/SPECS/xtreamui-nettle/xtreamui-nettle.spec -O $(rpm --eval %_topdir)/SPECS/xtream-ui-nettle.spec
-rpmbuild -ba $(rpm --eval %_topdir)/SPECS/xtream-ui-nettle.spec
-
-dnf -y install $(find $(rpm --eval %_topdir) -name 'xtream-ui-nettle-3.8-1.*.rpm')
-
-
-
-
-
-
-
-wget https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.19.0.tar.gz -O $(rpm --eval %_topdir)/SOURCES/libtasn1-4.19.0.tar.gz
-wget https://raw.githubusercontent.com/amidevous/ffmpegbuild/main/SPECS/xtream-ui-libtasn1.spec -O $(rpm --eval %_topdir)/SPECS/xtream-ui-libtasn1.spec
-rpmbuild -ba $(rpm --eval %_topdir)/SPECS/xtream-ui-libtasn1.spec
-dnf -y install $(find $(rpm --eval %_topdir) -name 'xtream-ui-libtasn1-4.19.0-1.el7.x86_64.rpm')
-wget https://github.com/libffi/libffi/releases/download/v3.4.4/libffi-3.4.4.tar.gz -O $(rpm --eval %_topdir)/SOURCES/libffi-3.4.4.tar.gz
-wget https://raw.githubusercontent.com/amidevous/ffmpegbuild/main/SPECS/xtream-ui-libffi.spec -O $(rpm --eval %_topdir)/SPECS/xtream-ui-libffi.spec
-rpmbuild -ba $(rpm --eval %_topdir)/SPECS/xtream-ui-libffi.spec
-dnf -y install $(find $(rpm --eval %_topdir) -name 'xtream-ui-libffi-3.4.4-1.el7.x86_64.rpm')
-wget https://github.com/p11-glue/p11-kit/releases/download/0.24.1/p11-kit-0.24.1.tar.xz -O $(rpm --eval %_topdir)/SOURCES/p11-kit-0.24.1.tar.xz
-wget https://raw.githubusercontent.com/amidevous/ffmpegbuild/main/SPECS/xtream-ui-p11-kit.spec -O $(rpm --eval %_topdir)/SPECS/xtream-ui-p11-kit.spec
-rpmbuild -ba $(rpm --eval %_topdir)/SPECS/xtream-ui-p11-kit.spec
-dnf -y install $(find $(rpm --eval %_topdir) -name 'xtream-ui-p11-kit-0.24.1-1.el7.x86_64.rpm')
-
-
-
-wget https://ftp.gnu.org/gnu/libunistring/libunistring-1.1.tar.xz -O $(rpm --eval %_topdir)/SOURCES/libunistring-1.1.tar.xz
-wget https://raw.githubusercontent.com/amidevous/ffmpegbuild/main/SPECS/xtream-ui-libunistring.spec -O $(rpm --eval %_topdir)/SPECS/xtream-ui-libunistring.spec
-rpmbuild -ba $(rpm --eval %_topdir)/SPECS/xtream-ui-libunistring.spec
-
-
-
-
-
-
-wget https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.bz2 -O $(rpm --eval %_topdir)/SOURCES/nasm-2.16.01.tar.bz2
-wget https://raw.githubusercontent.com/amidevous/ffmpegbuild/main/SPECS/xtream-ui-nasm.spec -O $(rpm --eval %_topdir)/SPECS/xtream-ui-nasm.spec
-rpmbuild -ba $(rpm --eval %_topdir)/SPECS/xtream-ui-nasm.spec
-dnf -y install $(find $(rpm --eval %_topdir) -name 'xtream-ui-nasm-2.16.01-1.el7.x86_64.rpm')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
